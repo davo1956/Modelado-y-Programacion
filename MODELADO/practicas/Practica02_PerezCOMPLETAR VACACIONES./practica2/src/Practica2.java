59aede337fe5344e1fdb5446c0395d7c499f9675
davo1956 <david1956@ciencias.unam.mx>
2020-03-25 15:08:29 -0600

    frist commit2

---
+++ MODELADO/practicas/Practica02_PerezCOMPLETAR VACACIONES./practica2/src/Practica2.java
@@ -0,0 +1,13 @@
+/**
+ * Practica Número 2 de Modelado y Programación.
+ */
+
+public class Practica2 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Inicio Simulacion");
+        
+    }
+
+}
